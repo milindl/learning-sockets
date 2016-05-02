7a35e33bccb732413274f8384ae7235ab0fbedf8
Milind L <milindl@iitk.ac.in>
2016-05-02 09:41:11 +0530

    Really simple chat client and server. Lame, but shows all the working principles

--- client_send_mes.py
+++ client_send_mes.py
@@ -1,26 +1,62 @@
-import socket, threading
+import socket, threading,tkinter
+
+sock=None
+txt_area=None
 #client, sender of messages
 #input_and_print_lock  = threading.Lock()
-def sender(sock):
+gui_setup_lock = threading.Lock()
+def send_mess(event):
+    global sock
+    message =  event.widget.get()
+    sender(sock,message)
+    event.widget.delete(0,tkinter.END)
 
-    message = ''
-    while message!='q\n':
 
-        #input_and_print_lock.acquire()
-        message = input('>>')
-        message = message + '\n'
-        #input_and_print_lock.release()
-        sock.send(message.encode())
+def sender(sock,message=''):
+    message = message + '\n'
+    sock.send(message.encode())
 
 
 def receiver(sock):
+    global txt_area
     while True:
         data = sock.recv(1024).decode()
         if not data:
             break
         #input_and_print_lock.acquire()
+
         print("Recvd message follows: " + data)
+        if txt_area:
+            print('yo ho ho and a bottle of rum')
+            txt_area.insert(tkinter.END,data)
+
         #input_and_print_lock.release()
+
+def setup_gui():
+    global txt_area
+    #gui_setup_lock.acquire()
+    #Setup the largest level widgets first
+    window = tkinter.Tk()
+    window.title("Chat Interface")
+    window.geometry('900x500')
+    upper_frame =  tkinter.Frame()
+    lower_frame = tkinter.Frame()
+    scroller = tkinter.Scrollbar(upper_frame)
+    main_txt = tkinter.Text(upper_frame, yscrollcommand=scroller.set, height=1000)
+    #main_txt.config(state=tkinter.DISABLED)
+    scroller.config(command=main_txt.yview)
+    entry = tkinter.Entry(lower_frame)
+    entry.bind('<Return>', send_mess)
+    entry.focus()
+    main_txt.pack(fill=tkinter.X, side=tkinter.TOP)
+    scroller.pack(side=tkinter.RIGHT, fill=tkinter.Y)
+    entry.pack(fill = tkinter.BOTH)
+    lower_frame.pack(fill=tkinter.X, side=tkinter.BOTTOM)
+    upper_frame.pack(fill=tkinter.X, side=tkinter.TOP)
+    txt_area=main_txt
+    main_txt.insert(tkinter.END,"Yo ho ho and a bottle of rum")
+    #gui_setup_lock.release()
+    window.mainloop()
 
 if __name__ == '__main__':
     host = '127.0.0.1'
@@ -28,10 +64,11 @@ if __name__ == '__main__':
 
     sock = socket.socket()
     sock.connect((host,port))
-    t1 = threading.Thread(target = sender, args = (sock,))
+    #t1 = threading.Thread(target = sender, args = (sock,))
     t2 = threading.Thread(target = receiver, args = (sock,))
-    t1.start()
     t2.start()
+    setup_gui()
+
     while threading.active_count() > 1:
         pass
     sock.close()
